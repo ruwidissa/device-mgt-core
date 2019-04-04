f4ec3e211cb2197866078338f630047bfafd7e7c
Jayasanka <jayasanka.sack@gmail.com>
2019-04-04 23:38:35 +0530

    added all form items to single page

--- components/application-mgt/org.wso2.carbon.device.application.mgt.publisher.ui/react-app/src/pages/dashboard/add-new-app/AddNewApp.js
+++ components/application-mgt/org.wso2.carbon.device.application.mgt.publisher.ui/react-app/src/pages/dashboard/add-new-app/AddNewApp.js
@@ -22,6 +22,8 @@ import Step1 from "./Step1"
 import Step2 from "./Step2"
 import Step3 from "./Step3"
 import styles from "./Style.less"
+import IconImage from "./IconImg"
+import UploadScreenshots from "./UploadScreenshots"
 
 const Paragraph = Typography;
 const Dragger = Upload.Dragger;
@@ -39,6 +41,23 @@ const routes = [
         breadcrumbName: 'add new app',
     },
 ];
+
+const props = {
+    name: 'file',
+    multiple: true,
+    action: '//jsonplaceholder.typicode.com/posts/',
+    onChange(info) {
+        const status = info.file.status;
+        if (status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
+    },
+};
 
 const Step = Steps.Step;
 
@@ -238,7 +257,35 @@ class AddNewApp extends React.Component {
                                             </Form>
                                         </div>
                                     </Col>
-                                    <Col span={12}>
+                                    <Col span={12} style={{paddingTop:40}}>
+                                        <p>Application</p>
+                                        <div style={{height:170}}>
+                                            <Dragger {...props}>
+                                                <p className="ant-upload-drag-icon">
+                                                    <Icon type="inbox" />
+                                                </p>
+                                                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                                <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
+                                            </Dragger>
+                                        </div>
+                                        <Row style={{marginTop:40}}>
+                                            <Col span={12}>
+                                                <p>Icon</p>
+                                                <IconImage/>
+                                            </Col>
+                                            <Col span={12}>
+                                                <p>Banner</p>
+                                                <IconImage/>
+                                            </Col>
+                                        </Row>
+
+
+                                        <Row style={{marginTop:40}}>
+                                            <Col span={24}>
+                                                <p>Screenshots</p>
+                                                <UploadScreenshots/>
+                                            </Col>
+                                        </Row>
 
                                     </Col>
                                 </Row>
