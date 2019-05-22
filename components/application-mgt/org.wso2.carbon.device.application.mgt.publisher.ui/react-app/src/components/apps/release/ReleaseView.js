753a3478489be989cd90e8b8e949360a9f9950b9
Jayasanka <jayasanka.sack@gmail.com>
2019-05-22 14:04:13 +0530

    Display screenshots and icon

--- components/application-mgt/org.wso2.carbon.device.application.mgt.publisher.ui/react-app/src/components/apps/release/ReleaseView.js
+++ components/application-mgt/org.wso2.carbon.device.application.mgt.publisher.ui/react-app/src/components/apps/release/ReleaseView.js
@@ -1,17 +1,18 @@
 import React from "react";
 import {Avatar, Row, Col, Typography} from "antd";
+import "../../../App.css";
 
 const {Title, Text} = Typography;
 
 class ReleaseView extends React.Component {
     render() {
         const release = this.props.release;
+        console.log(release);
         return (
-            <div>
+            <div className="release">
                 <Row>
-                    <Col span={4}>
-                        <Avatar size={128} shape="square"
-                                src={release.iconPath}/>
+                    <Col span={4} className="release-icon">
+                        <img src={release.iconPath}/>
                     </Col>
                     <Col span={18}>
                         <Title level={2}>App Name</Title>
@@ -21,8 +22,14 @@ class ReleaseView extends React.Component {
                 </Row>
                 <br/>
                 <Row>
-                    <Col span={6}>
-                        <img style={{width:"100%"}} src={release.screenshotPath1}/>
+                    <Col span={6} className="release-screenshot">
+                        <img src={release.screenshotPath1}/>
+                    </Col>
+                    <Col span={6} className="release-screenshot">
+                        <img src={release.screenshotPath2}/>
+                    </Col>
+                    <Col span={6} className="release-screenshot">
+                        <img src={release.screenshotPath3}/>
                     </Col>
                 </Row>
             </div>
--- components/application-mgt/org.wso2.carbon.device.application.mgt.publisher.ui/react-app/src/pages/dashboard/Dashboard.js
+++ components/application-mgt/org.wso2.carbon.device.application.mgt.publisher.ui/react-app/src/pages/dashboard/Dashboard.js
@@ -3,12 +3,11 @@ import {Layout, Menu, Icon} from 'antd';
 
 const {Header, Content, Footer} = Layout;
 
-import styles from './Dashboard.less';
 import Logo from "../../../public/images/logo.svg";
 import {Link, NavLink} from "react-router-dom";
 import RouteWithSubRoutes from "../../components/RouteWithSubRoutes"
-import { Switch, Redirect } from 'react-router'
-
+import {Switch, Redirect} from 'react-router'
+import "../../App.css";
 
 class Dashboard extends React.Component {
     constructor(props) {
@@ -20,36 +19,42 @@ class Dashboard extends React.Component {
 
     render() {
         return (
-            <Layout className="layout">
-                <Header>
-                    <div className={styles.logo}>
-                        <img src={Logo}/>
-                    </div>
-                    <Menu
-                        theme="light"
-                        mode="horizontal"
-                        defaultSelectedKeys={['2']}
-                        style={{lineHeight: '64px'}}
-                    >
-                        <Menu.Item key="1"><Link to="/publisher/apps"><Icon type="appstore"/>Apps</Link></Menu.Item>
-                        <Menu.Item key="2"><Link to="/publisher/apps"><Icon type="line-chart"/>Apps</Link></Menu.Item>
-                        <Menu.Item key="3"><Link to="/publisher/apps/new-app"><Icon type="upload"/>Add New App</Link></Menu.Item>
-                    </Menu>
-                </Header>
-                <Content style={{padding: '0 0'}}>
-                    <Switch>
-                        <Redirect exact from="/publisher" to="/publisher/apps"/>
-                        {this.state.routes.map((route) => (
-                            <RouteWithSubRoutes key={route.path} {...route} />
-                        ))}
+            <div>
+                <Layout className="layout">
+                    <Header>
+                        <div className="logo">
+                            <img src={Logo}/>
+                        </div>
+                        <Menu
+                            theme="light"
+                            mode="horizontal"
+                            defaultSelectedKeys={['2']}
+                            style={{lineHeight: '64px'}}
+                        >
+                            <Menu.Item key="1"><Link to="/publisher/apps"><Icon type="appstore"/>Apps</Link></Menu.Item>
+                            <Menu.Item key="2"><Link to="/publisher/apps"><Icon
+                                type="line-chart"/>Apps</Link></Menu.Item>
+                            <Menu.Item key="3"><Link to="/publisher/apps/new-app"><Icon type="upload"/>Add New
+                                App</Link></Menu.Item>
+                        </Menu>
+                    </Header>
+                </Layout>
+                <Layout>
+                    <Content style={{padding: '0 0'}}>
+                        <Switch>
+                            <Redirect exact from="/publisher" to="/publisher/apps"/>
+                            {this.state.routes.map((route) => (
+                                <RouteWithSubRoutes key={route.path} {...route} />
+                            ))}
 
-                    </Switch>
+                        </Switch>
 
-                </Content>
-                <Footer style={{textAlign: 'center'}}>
-                    ©2019 entgra.io
-                </Footer>
-            </Layout>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>
+                        ©2019 entgra.io
+                    </Footer>
+                </Layout>
+            </div>
         );
     }
 }
