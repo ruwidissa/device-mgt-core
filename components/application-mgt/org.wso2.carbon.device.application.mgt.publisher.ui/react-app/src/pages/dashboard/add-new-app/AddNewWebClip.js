f1c99a5b749ca8b1bd62107cab88e6ed91bc07c9
Jayasanka <jayasanka.sack@gmail.com>
2019-06-25 15:08:59 +0530

    Create dynamic component to create application

---
+++ components/application-mgt/org.wso2.carbon.device.application.mgt.publisher.ui/react-app/src/pages/dashboard/add-new-app/AddNewWebClip.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "antd/dist/antd.css";
+import {
+    PageHeader,
+    Typography
+} from "antd";
+import AddNewAppForm from "../../../components/new-app/AddNewAppForm";
+
+const Paragraph = Typography;
+
+const formConfig = {
+    installationType: "WEB_CLIP",
+    endpoint: "/web-app",
+    releaseWrapperName: "webClipReleaseWrappers",
+    specificElements: {
+        url : {
+            required: true
+        }
+    }
+};
+
+class AddNewEnterpriseApp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            current: 0,
+            categories: []
+        };
+    }
+
+    componentDidMount() {
+        // this.getCategories();
+    }
+
+
+    render() {
+        return (
+            <div>
+                <PageHeader
+                    title="Add New Web Clip"
+                >
+                    <div className="wrap">
+                        <div className="content">
+                            <Paragraph>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo.
+                            </Paragraph>
+                        </div>
+                    </div>
+                </PageHeader>
+                <div style={{background: '#f0f2f5', padding: 24, minHeight: 720}}>
+                    <AddNewAppForm formConfig={formConfig}/>
+                </div>
+
+            </div>
+
+        );
+    }
+}
+
+export default AddNewEnterpriseApp;
