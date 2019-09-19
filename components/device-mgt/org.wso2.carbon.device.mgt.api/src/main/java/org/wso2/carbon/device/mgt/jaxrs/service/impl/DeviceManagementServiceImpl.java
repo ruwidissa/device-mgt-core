647099f8a9099095f2944ea6906225484cbdbd4a
Ace <ruwany.ace@gmail.com>
2019-09-19 10:22:47 +0530

    Adding debug logs to query-devices API

--- components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/service/impl/DeviceManagementServiceImpl.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/service/impl/DeviceManagementServiceImpl.java
@@ -653,11 +653,17 @@ public class DeviceManagementServiceImpl implements DeviceManagementService {
         DeviceList deviceList = new DeviceList();
         try {
             if(map.getProperties().isEmpty()){
+                if (log.isDebugEnabled()) {
+                    log.debug("No search criteria defined when querying devices.");
+                }
                 return Response.status(Response.Status.BAD_REQUEST).entity("No search criteria defined.").build();
             }
             DeviceManagementProviderService dms = DeviceMgtAPIUtils.getDeviceManagementService();
             devices = dms.getDevicesBasedOnProperties(map.getProperties());
             if(devices == null || devices.isEmpty()){
+                if (log.isDebugEnabled()) {
+                    log.debug("No Devices Found for criteria : " + map);
+                }
                 return Response.status(Response.Status.NOT_FOUND).entity("No device found matching query criteria.").build();
             }
         } catch (DeviceManagementException e) {
