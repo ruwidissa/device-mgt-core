8ff264a044f165f298c482a45006756a50f6de2e
Shamalka Navod <shamalkanavod95@gmail.com>
2019-09-26 12:23:10 +0000

    Fix error logs of DAO implementations

--- components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/dao/impl/device/SQLServerDeviceDAOImpl.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/dao/impl/device/SQLServerDeviceDAOImpl.java
@@ -424,8 +424,10 @@ public class SQLServerDeviceDAOImpl extends AbstractDeviceDAOImpl {
                 devices.add(device);
             }
         } catch (SQLException e) {
-            throw new DeviceManagementDAOException("Error occurred while fetching the list of devices that matches to status " +
-                                                   "'" + request.getStatus() + "'", e);
+            String msg = "Error occurred while fetching the list of devices that matches to status " +
+                         "'" + request.getStatus() + "'";
+            log.error(msg, e);
+            throw new DeviceManagementDAOException(msg, e);
         } finally {
             DeviceManagementDAOUtil.cleanupResources(stmt, null);
         }
@@ -489,8 +491,10 @@ public class SQLServerDeviceDAOImpl extends AbstractDeviceDAOImpl {
                 devices.add(device);
             }
         } catch (SQLException e) {
-            throw new DeviceManagementDAOException("Error occurred while fetching the list of devices corresponding" +
-                    "to the mentioned filtering criteria", e);
+            String msg = "Error occurred while fetching the list of devices corresponding" +
+                         "to the mentioned filtering criteria";
+            log.error(msg, e);
+            throw new DeviceManagementDAOException(msg, e);
         } finally {
             DeviceManagementDAOUtil.cleanupResources(stmt, rs);
         }
