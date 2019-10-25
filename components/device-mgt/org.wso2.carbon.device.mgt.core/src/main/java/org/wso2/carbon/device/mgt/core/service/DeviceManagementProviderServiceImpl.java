d6845cd40a0438356fd38c959b748502b8b5f943
charitha <charitha@entgra.io>
2019-10-26 01:03:44 +0530

    Add new API endpoints - Change device status as bulk - Get total device count - Add group with different owner

--- components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/service/DeviceManagementProviderServiceImpl.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/service/DeviceManagementProviderServiceImpl.java
@@ -3125,10 +3125,10 @@ public class DeviceManagementProviderServiceImpl implements DeviceManagementProv
     }
 
     @Override
-    public int getDeviceCountOfTypeByStatus(int tenantId, String deviceType, String deviceStatus) throws DeviceManagementException {
+    public int getDeviceCountOfTypeByStatus(String deviceType, String deviceStatus) throws DeviceManagementException {
         try {
             DeviceManagementDAOFactory.openConnection();
-            return deviceDAO.getDeviceCount(deviceType, deviceStatus, tenantId);
+            return deviceDAO.getDeviceCount(deviceType, deviceStatus, getTenantId());
         } catch (DeviceManagementDAOException e) {
             String msg = "Error occurred in while retrieving device count by status for deviceType :" +deviceType + " status : " + deviceStatus;
             log.error(msg, e);
@@ -3143,11 +3143,11 @@ public class DeviceManagementProviderServiceImpl implements DeviceManagementProv
     }
 
     @Override
-    public List<String> getDeviceIdentifiersByStatus(int tenantId, String deviceType, String deviceStatus) throws DeviceManagementException {
+    public List<String> getDeviceIdentifiersByStatus(String deviceType, String deviceStatus) throws DeviceManagementException {
         List<String> deviceIds;
         try {
             DeviceManagementDAOFactory.openConnection();
-            deviceIds = deviceDAO.getDeviceIdentifiers(deviceType, deviceStatus, tenantId);
+            deviceIds = deviceDAO.getDeviceIdentifiers(deviceType, deviceStatus, getTenantId());
         } catch (DeviceManagementDAOException e) {
             String msg = "Error occurred in while retrieving devices by status for deviceType :" +deviceType + " status : " + deviceStatus;
             log.error(msg, e);
@@ -3163,17 +3163,16 @@ public class DeviceManagementProviderServiceImpl implements DeviceManagementProv
     }
 
     @Override
-    public boolean bulkUpdateDeviceStatus(int tenantId, String deviceType,
-                                          List<String> deviceList, String status)
+    public boolean bulkUpdateDeviceStatus(String deviceType, List<String> deviceList, String status)
             throws DeviceManagementException {
         boolean success;
         try {
             DeviceManagementDAOFactory.openConnection();
-            success = deviceDAO.setEnrolmentStatusInBulk(deviceType, status, tenantId, deviceList);
+            success = deviceDAO.setEnrolmentStatusInBulk(deviceType, status, getTenantId(), deviceList);
             DeviceManagementDAOFactory.commitTransaction();
         } catch (DeviceManagementDAOException e) {
-            String msg = "Error occurred in while updating status of devices :" + deviceType + " status : " + deviceList
-                    .toString();
+            DeviceManagementDAOFactory.rollbackTransaction();
+            String msg = "Error occurred in while updating status of devices :" + deviceType + " status : " + status;
             log.error(msg, e);
             throw new DeviceManagementException(msg, e);
         } catch (SQLException e) {
