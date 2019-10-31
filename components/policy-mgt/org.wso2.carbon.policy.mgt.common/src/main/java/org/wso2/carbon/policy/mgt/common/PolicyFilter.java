5329508fb3d32a31773c9661a086f6463386d0b6
Saad Sahibjan <saadsahibjan@gmail.com>
2019-10-31 13:56:06 +0530

    Add general policy filter to delegate only general policies

--- components/policy-mgt/org.wso2.carbon.policy.mgt.common/src/main/java/org/wso2/carbon/policy/mgt/common/PolicyFilter.java
+++ components/policy-mgt/org.wso2.carbon.policy.mgt.common/src/main/java/org/wso2/carbon/policy/mgt/common/PolicyFilter.java
@@ -14,6 +14,23 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
+ *
+ * Copyright (c) 2019, Entgra (Pvt) Ltd. (http://entgra.io) All Rights Reserved.
+ *
+ * Entgra (Pvt) Ltd. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 
@@ -28,6 +45,13 @@ import java.util.Map;
 public interface PolicyFilter {
 
     List<Policy> filterActivePolicies(List<Policy> policies);
+
+    /**
+     * Filter and retrieve all the general policies out of active policies
+     * @param policies contains a list of active policies
+     * @return a list of {@link Policy} general policies
+     */
+    List<Policy> filterGeneralPolicies(List<Policy> policies);
 
     List<Policy> filterDeviceGroupsPolicies(Map<Integer, DeviceGroup> groupMap, List<Policy> policies);
 
