7599d97e68afa9a5f7042d3793b2ef9d7c28c672
Saad Sahibjan <saadsahibjan@gmail.com>
2020-03-04 13:12:54 +0530

    Call reporting app usage endpoint

--- components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/device/details/mgt/impl/DeviceInformationManagerImpl.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/device/details/mgt/impl/DeviceInformationManagerImpl.java
@@ -18,6 +18,7 @@
 
 package org.wso2.carbon.device.mgt.core.device.details.mgt.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.context.CarbonContext;
