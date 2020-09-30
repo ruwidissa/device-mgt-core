ac41aa7119e743a00cd70d68358b3f4047dc8ab3
Kaveesha <mhkmihirangi@gmail.com>
2020-09-30 13:35:07 +0530

    Add APISelect class to policy mgt ui pack

--- components/device-mgt/org.wso2.carbon.device.mgt.common/src/main/java/org/wso2/carbon/device/mgt/common/policy/mgt/ui/Item.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.common/src/main/java/org/wso2/carbon/device/mgt/common/policy/mgt/ui/Item.java
@@ -45,6 +45,7 @@ public class Item {
     private InputList inputList;
     private String nullableValue;
     private String divider;
+    private APISelect apiSelect;
 
     @XmlElement(name = "Label")
     public String getLabel() {
@@ -206,5 +207,14 @@ public class Item {
 
     public void setDivider(String divider) {
         this.divider = divider;
+    }
+
+    @XmlElement(name = "APISelect")
+    public APISelect getApiSelect() {
+        return apiSelect;
+    }
+
+    public void setApiSelect(APISelect apiSelect) {
+        this.apiSelect = apiSelect;
     }
 }
