97bb7ee87cb3bccd37ad119945b2d1eac3f0f49d
Saad Sahibjan <saadsahibjan@gmail.com>
2021-02-15 22:06:32 +0530

    Add SMS management common classes

---
+++ components/sms-mgt/io.entgra.sms.mgt.common/src/main/java/io/entgra/sms/mgt/common/SMSMgtConstants.java
@@ -0,0 +1,10 @@
+package io.entgra.sms.mgt.common;
+
+/**
+ * This class holds SMS Management Constants
+ */
+public final class SMSMgtConstants {
+
+    public static final String SCOPE = "scope";
+    public static final String SMS_CONFIG_XML_NAME = "sms-config.xml";
+}
