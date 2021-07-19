eb13d1b70dfc5c1c79bc86c8c36877f28d2e5204
Dharmakeerthi Lasantha <tcdlpds@gmail.com>
2021-07-19 10:18:32 +0000

    Revert "Merge branch 'master' into 'master'"
    This reverts merge request !785

--- components/application-mgt/org.wso2.carbon.device.application.mgt.core/src/main/java/org/wso2/carbon/device/application/mgt/core/impl/SubscriptionManagerImpl.java
+++ components/application-mgt/org.wso2.carbon.device.application.mgt.core/src/main/java/org/wso2/carbon/device/application/mgt/core/impl/SubscriptionManagerImpl.java
@@ -848,16 +848,16 @@ public class SubscriptionManagerImpl implements SubscriptionManager {
                     }
                 }
 
+                subscriptionDAO.addDeviceSubscription(username, subInsertingDeviceIds, subType,
+                        Operation.Status.PENDING.toString(), applicationReleaseId, tenantId);
                 if (!subUpdatingDeviceIds.isEmpty()) {
                     subscriptionDAO.updateDeviceSubscription(username, subUpdatingDeviceIds, action, subType,
                             Operation.Status.PENDING.toString(), applicationReleaseId, tenantId);
                 }
-
-                if (!subInsertingDeviceIds.isEmpty()) {
-                    subscriptionDAO.addDeviceSubscription(username, subInsertingDeviceIds, subType,
-                            Operation.Status.PENDING.toString(), applicationReleaseId, tenantId);
+                subUpdatingDeviceIds.addAll(subInsertingDeviceIds);
+                if (!subUpdatingDeviceIds.isEmpty()) {
                     List<Integer> deviceSubIds = new ArrayList<>(
-                            subscriptionDAO.getDeviceSubIds(subInsertingDeviceIds, applicationReleaseId, tenantId));
+                            subscriptionDAO.getDeviceSubIds(subUpdatingDeviceIds, applicationReleaseId, tenantId));
                     subscriptionDAO.addOperationMapping(operationId, deviceSubIds, tenantId);
                 }
             }
