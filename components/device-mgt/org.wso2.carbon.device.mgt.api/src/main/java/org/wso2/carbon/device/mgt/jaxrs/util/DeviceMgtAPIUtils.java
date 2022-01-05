fd821629097e9986d55325999dcd1026c7a5fa92
Rushdi <rushdi@entgra.io>
2022-01-05 13:04:42 +0530

    Added new operation status

--- components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/util/DeviceMgtAPIUtils.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/util/DeviceMgtAPIUtils.java
@@ -959,9 +959,15 @@ public class DeviceMgtAPIUtils {
                 case Constants.OperationStatus.REPEATED:
                     operation.setStatus(Operation.Status.REPEATED);
                     break;
+                case Constants.OperationStatus.REQUIRE_CONFIRMATION:
+                    operation.setStatus(Operation.Status.REQUIRE_CONFIRMATION);
+                    break;
+                case Constants.OperationStatus.CONFIRMED:
+                    operation.setStatus(Operation.Status.CONFIRMED);
+                    break;
                 default:
                     String msg = "Invalid operation status. Valid operations: " +
-                            "[IN_PROGRESS, PENDING, COMPLETED, ERROR, REPEATED, NOTNOW]";
+                            "[IN_PROGRESS, PENDING, COMPLETED, ERROR, REPEATED, NOTNOW, REQUIRE_CONFIRMATION, CONFIRMED]";
                     log.error(msg);
                     throw new BadRequestException(msg);
             }
