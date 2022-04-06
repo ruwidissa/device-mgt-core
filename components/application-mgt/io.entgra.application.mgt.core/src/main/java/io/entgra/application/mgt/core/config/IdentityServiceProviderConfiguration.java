daaeed52e29e8936942038c0f222d2cc005b5044
Mohamed Rashd <rashad@entgra.io>
2022-04-06 11:08:33 +0530

    Add and Change licence from WSO2 to Entgra

--- components/application-mgt/io.entgra.application.mgt.core/src/main/java/io/entgra/application/mgt/core/config/IdentityServiceProviderConfiguration.java
+++ components/application-mgt/io.entgra.application.mgt.core/src/main/java/io/entgra/application/mgt/core/config/IdentityServiceProviderConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Entgra (pvt) Ltd. (http://entgra.io) All Rights Reserved.
+ * Copyright (c) 2022, Entgra (pvt) Ltd. (http://entgra.io) All Rights Reserved.
  *
  * Entgra (pvt) Ltd. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package io.entgra.application.mgt.core.config;
 
 import javax.xml.bind.annotation.XmlElement;
